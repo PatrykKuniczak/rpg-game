0f533343cc926c871814c65ef2243c17da996f39
Patryk Kuniczak <p.kuniczak@gmail.com>
2024-02-18 15:02:59 +0100

    feat(core): create abstract Model

---
+++ src/core/Model.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\core;
+
+abstract class Model
+{
+    const string TABLE_NAME = EntitiesEnum::Character->name;
+
+    public function create(object $value): object
+    {
+        return Database::create(self::TABLE_NAME, $value);
+    }
+
+    public function delete(string $column, string $value): string
+    {
+        return Database::delete(self::TABLE_NAME, $column, $value);
+    }
+
+    public function findAll(): array
+    {
+        return Database::findAll(self::TABLE_NAME);
+    }
+
+    public function edit(string $column, string $oldValue, string $newValue): string
+    {
+        return Database::edit(self::TABLE_NAME, $column, $oldValue, $newValue);
+    }
+
+    public function findOneBy(string $column, string $value): object|string
+    {
+        return Database::findOneBy(self::TABLE_NAME, $column, $value);
+    }
+}
+
